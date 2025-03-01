114c80e89da4391ab955f9bffa6b7a2e75784699
DevBrianKE <kipchumbabrian47@gmail.com>
2025-03-01 14:22:01 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+dodger.style.background = "#FF69B4";
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+  
+  const gameFieldWidth = 400; // The game field width is 400px
+  const dodgerWidth = 40; // The dodger is 40px wide
+
+  if (left < gameFieldWidth - dodgerWidth) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
